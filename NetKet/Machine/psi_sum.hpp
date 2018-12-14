8aeacbe89c30217157c50a20d52f5c9cb127c04a
kenny_choo <kenny.choo@uzh.ch>
2018-12-14 11:17:52 +0100

    with global phase rotation

--- NetKet/Machine/psi_sum.hpp
+++ NetKet/Machine/psi_sum.hpp
@@ -15,6 +15,7 @@
 #ifndef NETKET_PSISUM_HPP
 #define NETKET_PSISUM_HPP
 
+#include <cmath>
 #include <fstream>
 #include <memory>
 
@@ -35,15 +36,22 @@ class PsiSum : public AbstractMachine<T> {
   using Ptype = std::unique_ptr<AbstractMachine<T>>;
 
   int nv_;
+  int l_;
 
   Eigen::VectorXcd alpha_;
 
   Ptype psi1_;
   Ptype psi2_;
 
+  Eigen::VectorXd rot1_;
+  Eigen::VectorXd rot2_;
+
   const Hamiltonian &ham_;
   const Hilbert &hilbert_;
   const Graph &graph_;
+
+  const std::complex<double> I;
+  const double pi_;
 
  public:
   using VectorType = typename AbstractMachine<T>::VectorType;
@@ -53,16 +61,38 @@ class PsiSum : public AbstractMachine<T> {
 
   explicit PsiSum(const Graph &graph, const Hamiltonian &hamiltonian,
                   const json &pars)
-      : ham_(hamiltonian), hilbert_(hamiltonian.GetHilbert()), graph_(graph) {
+      : ham_(hamiltonian),
+        hilbert_(hamiltonian.GetHilbert()),
+        graph_(graph),
+        I(0.0, 1.0),
+        pi_(std::acos(-1)) {
     nv_ = hilbert_.Size();
     alpha_.resize(2);
     alpha_.setConstant(1.0);
+    alpha_(0) = 0.1;
+
+    l_ = FieldVal(pars["SumMachine"], "Length");
+
+    rot1_.resize(nv_);
+    rot1_.setZero();
+    rot2_.resize(nv_);
+    rot2_.setZero();
+
+    for (int i = 0; i < nv_; ++i) {
+      Eigen::VectorXi coord = Site2Coord(i, l_);
+      if (coord(0) % 2 == 0) {
+        rot1_(i) = 1;
+      }
+      if (coord(1) % 2 == 0) {
+        rot2_(i) = 1;
+      }
+    }
 
     InfoMessage() << "Machine 1 is ";
-    psi1_ = Init(graph_, hilbert_, pars["SumMachine"][0]);
+    psi1_ = Init(graph_, hilbert_, pars["SumMachine"]["Machines"][0]);
 
     InfoMessage() << "Machine 2 is ";
-    psi2_ = Init(graph_, hilbert_, pars["SumMachine"][1]);
+    psi2_ = Init(graph_, hilbert_, pars["SumMachine"]["Machines"][1]);
 
     InitParameters(pars);
 
@@ -94,10 +124,11 @@ class PsiSum : public AbstractMachine<T> {
   }
 
   void InitParameters(const json &pars) {
-    psi1_->InitRandomPars(pars["SumMachine"][0]);
+    psi1_->InitRandomPars(pars["SumMachine"]["Machines"][0]);
 
-    if (FieldExists(pars["SumMachine"][0]["Machine"], "InitFile")) {
-      std::string filename = pars["SumMachine"][0]["Machine"]["InitFile"];
+    if (FieldExists(pars["SumMachine"]["Machines"][0]["Machine"], "InitFile")) {
+      std::string filename =
+          pars["SumMachine"]["Machines"][0]["Machine"]["InitFile"];
 
       std::ifstream ifs(filename);
 
@@ -113,10 +144,11 @@ class PsiSum : public AbstractMachine<T> {
       }
     }
 
-    psi2_->InitRandomPars(pars["SumMachine"][1]);
+    psi2_->InitRandomPars(pars["SumMachine"]["Machines"][1]);
 
-    if (FieldExists(pars["SumMachine"][1]["Machine"], "InitFile")) {
-      std::string filename = pars["SumMachine"][1]["Machine"]["InitFile"];
+    if (FieldExists(pars["SumMachine"]["Machines"][1]["Machine"], "InitFile")) {
+      std::string filename =
+          pars["SumMachine"]["Machines"][1]["Machine"]["InitFile"];
 
       std::ifstream ifs(filename);
 
@@ -173,6 +205,14 @@ class PsiSum : public AbstractMachine<T> {
 
   // Value of the logarithm of the wave-function
   T LogVal(const Eigen::VectorXd &v) override {
+    // double p1 =
+    //     ((int)std::round((rot1_.dot(v) + nv_ / 2) / 2)) % 2 == 0 ? 1.0 :
+    //     -1.0;
+    // double p2 =
+    //     ((int)std::round((rot2_.dot(v) + nv_ / 2) / 2)) % 2 == 0 ? 1.0 :
+    //     -1.0;
+    // return std::log(p1 * alpha_[0] * std::exp(psi1_->LogVal(v)) +
+    //                 p2 * alpha_[1] * std::exp(psi2_->LogVal(v)));
     return std::log(alpha_[0] * std::exp(psi1_->LogVal(v)) +
                     alpha_[1] * std::exp(psi2_->LogVal(v)));
   }
@@ -188,6 +228,15 @@ class PsiSum : public AbstractMachine<T> {
   VectorType DerLog(const Eigen::VectorXd &v) override {
     std::complex<double> p1 = std::exp(psi1_->LogVal(v));
     std::complex<double> p2 = std::exp(psi2_->LogVal(v));
+    // double ph1 =
+    //     ((int)std::round((rot1_.dot(v) + nv_ / 2) / 2)) % 2 == 0 ? 1.0 :
+    //     -1.0;
+    // double ph2 =
+    //     ((int)std::round((rot2_.dot(v) + nv_ / 2) / 2)) % 2 == 0 ? 1.0 :
+    //     -1.0;
+    // p1 *= ph1;
+    // p2 *= ph2;
+
     VectorType der(2);
 
     der(0) = p1 / (alpha_[0] * p1 + alpha_[1] * p2);
@@ -222,7 +271,6 @@ class PsiSum : public AbstractMachine<T> {
         }
       }
     }
-
     return logvaldiffs;
   }
 
@@ -253,6 +301,25 @@ class PsiSum : public AbstractMachine<T> {
       alpha_ = j["Machine"]["alpha"];
       InfoMessage() << "Lanczos parameters initialized " << std::endl;
     }
+  }
+
+  int Coord2Site(Eigen::VectorXi const &coord, int L) {
+    auto site = 0;
+    auto scale = 1;
+    for (int i = 0; i < 2; ++i) {
+      site += scale * coord(i);
+      scale *= L;
+    }
+    return site;
+  }
+
+  Eigen::VectorXi Site2Coord(int site, int L) {
+    Eigen::VectorXi coord(2);
+    for (int i = 0; i < 2; ++i) {
+      coord(i) = site % L;
+      site /= L;
+    }
+    return coord;
   }
 };
 }  // namespace netket
