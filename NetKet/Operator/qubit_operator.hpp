ad3e5331bc7df719bbf12b7551967e16a99ed224
kenny_choo <kenny.choo@uzh.ch>
2019-05-09 11:08:21 +0200

    increase precision

--- NetKet/Operator/qubit_operator.hpp
+++ NetKet/Operator/qubit_operator.hpp
@@ -171,7 +171,7 @@ class QubitOperator : public AbstractOperator {
         }
         mel_temp += m_temp;
       }
-      if (std::abs(mel_temp) > 0.001) {
+      if (std::abs(mel_temp) > 1e-10) {
         std::vector<double> newconf_temp(tochange2_[i].size());
         int jj = 0;
         for (auto sj : tochange2_[i]) {
